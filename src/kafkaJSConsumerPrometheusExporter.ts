e5530a44c10df0e4a72e70a1c22fe6038ffa8ae3
Christian Galsterer <christian.galsterer@gmx.de>
2023-08-13 22:15:39 +0200

    feat: implement poc for consumer connection metric

--- src/kafkaJSConsumerPrometheusExporter.ts
+++ src/kafkaJSConsumerPrometheusExporter.ts
@@ -1,28 +1,41 @@
-import { type Registry, Gauge } from 'prom-client'
-import { type ConnectEvent, type Consumer } from 'kafkajs'
+import { type Registry, Gauge, Counter } from 'prom-client'
+import { type DisconnectEvent, type ConnectEvent, type Consumer } from 'kafkajs'
 
 export class KafkaJSConsumerPrometheusExporter {
   private readonly consumer: Consumer
   private readonly register: Registry
 
   private readonly consumerConnections: Gauge
+  private readonly consumerConnectionsTotal: Counter
 
   constructor (consumer: Consumer, register: Registry) {
     this.consumer = consumer
     this.register = register
 
     this.consumerConnections = new Gauge({
-      name: 'kafka_consumer_connections',
+      name: 'kafka_consumer_connection_count',
       help: 'the current connections established with a broker',
+      registers: [this.register]
+    })
+
+    this.consumerConnectionsTotal = new Counter({
+      name: 'kafka_consumer_connection_total',
+      help: 'the number of connections established with a broker',
       registers: [this.register]
     })
   }
 
   public enableMetrics (): void {
     this.consumer.on('consumer.connect', event => { this.onConsumerConnect(event) })
+    this.consumer.on('consumer.disconnect', event => { this.onConsumerDisconnect(event) })
   }
 
   onConsumerConnect (event: ConnectEvent): void {
     this.consumerConnections.inc()
+    this.consumerConnectionsTotal.inc()
+  }
+
+  onConsumerDisconnect (event: DisconnectEvent): void {
+    this.consumerConnections.dec()
   }
 }
