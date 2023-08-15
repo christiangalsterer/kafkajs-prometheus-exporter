36af11e910121fd92e9dcc941c830fa969a01fe4
Christian Galsterer <christian.galsterer@gmx.de>
2023-08-15 07:15:39 +0200

    feat: implement poc for consumer connection metric

--- src/kafkaJSConsumerPrometheusExporter.ts
+++ src/kafkaJSConsumerPrometheusExporter.ts
@@ -3,6 +3,7 @@ import { type DisconnectEvent, type ConnectEvent, type Consumer, type ConsumerCr
 
 export class KafkaJSConsumerPrometheusExporter {
   private readonly consumer: Consumer
+  private readonly clientId: string
   private readonly register: Registry
 
   private readonly consumerActiveConnections: Gauge
@@ -10,31 +11,35 @@ export class KafkaJSConsumerPrometheusExporter {
   private readonly consumerConnectionsClosedTotal: Counter
   private readonly consumerConnectionsCrashedTotal: Counter
 
-  constructor (consumer: Consumer, register: Registry) {
+  constructor (consumer: Consumer, clientId: string, register: Registry) {
     this.consumer = consumer
+    this.clientId = clientId
     this.register = register
 
     this.consumerActiveConnections = new Gauge({
       name: 'kafka_consumer_connections',
-      help: 'The current number of connections established with a broker',
+      help: 'The current number of active connections established with a broker',
+      labelNames: ['client_id'],
       registers: [this.register]
     })
 
     this.consumerConnectionsCreatedTotal = new Counter({
       name: 'kafka_consumer_connection_creation_total',
       help: 'The total number of connections established with a broker',
+      labelNames: ['client_id'],
       registers: [this.register]
     })
 
     this.consumerConnectionsClosedTotal = new Counter({
       name: 'kafka_consumer_connection_close_total',
       help: 'The total number of connections closed with a broker',
+      labelNames: ['client_id'],
       registers: [this.register]
     })
 
     this.consumerConnectionsCrashedTotal = new Counter({
       name: 'kafka_consumer_connection_crashed_total',
-      help: 'The total number of crashed connections closed with a broker',
+      help: 'The total number of crashed connections with a broker',
       labelNames: ['client_id', 'error', 'restart'],
       registers: [this.register]
     })
@@ -47,13 +52,13 @@ export class KafkaJSConsumerPrometheusExporter {
   }
 
   onConsumerConnect (event: ConnectEvent): void {
-    this.consumerActiveConnections.inc()
-    this.consumerConnectionsCreatedTotal.inc()
+    this.consumerActiveConnections.inc({ client_id: this.clientId })
+    this.consumerConnectionsCreatedTotal.inc({ client_id: this.clientId })
   }
 
   onConsumerDisconnect (event: DisconnectEvent): void {
-    this.consumerActiveConnections.dec()
-    this.consumerConnectionsClosedTotal.inc()
+    this.consumerActiveConnections.dec({ client_id: this.clientId })
+    this.consumerConnectionsClosedTotal.inc({ client_id: this.clientId })
   }
 
   onConsumerCrashed (event: ConsumerCrashEvent): void {
--- src/monitorKafkaJS.ts
+++ src/monitorKafkaJS.ts
@@ -2,7 +2,7 @@ import { type Registry } from 'prom-client'
 import { type Consumer } from 'kafkajs'
 import { KafkaJSConsumerPrometheusExporter } from './kafkaJSConsumerPrometheusExporter'
 
-export function monitorKafkaJS (consumer: Consumer, register: Registry): void {
-  const exporter = new KafkaJSConsumerPrometheusExporter(consumer, register)
+export function monitorKafkaJS (consumer: Consumer, clientId: string, register: Registry): void {
+  const exporter = new KafkaJSConsumerPrometheusExporter(consumer, clientId, register)
   exporter.enableMetrics()
 }
