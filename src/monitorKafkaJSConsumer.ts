4307a559f700d37725c99e56caec698994f6c447
Christian Galsterer <christian.galsterer@gmx.de>
2023-08-29 11:42:51 +0200

    docs: added documentation for public contract

--- src/monitorKafkaJSConsumer.ts
+++ src/monitorKafkaJSConsumer.ts
@@ -3,6 +3,14 @@ import { type Consumer } from 'kafkajs'
 import { KafkaJSConsumerPrometheusExporter } from './kafkaJSConsumerPrometheusExporter'
 import { type KafkaJSConsumerExporterOptions } from './kafkaJSConsumerExporterOptions'
 
+/**
+ * Exposes metrics for a KafkaJS consumer in prometheus format.
+ *
+ * @param consumer The KafkaS consumer to monitor.
+ * @param clientId The client id for the application. The client id is identical across different instances of the same application.
+ * @param register The prometheus registry used to expose the metrics.
+ * @param options Optional parameter to configure the exporter
+ */
 export function monitorKafkaJSConsumer (consumer: Consumer, clientId: string, register: Registry, options?: KafkaJSConsumerExporterOptions): void {
   const exporter = new KafkaJSConsumerPrometheusExporter(consumer, clientId, register, options)
   exporter.enableMetrics()
--- src/monitorKafkaJSProducer.ts
+++ src/monitorKafkaJSProducer.ts
@@ -3,6 +3,14 @@ import { type Producer } from 'kafkajs'
 import { KafkaJSProducerPrometheusExporter } from './kafkaJSProducerPrometheusExporter'
 import { type KafkaJSProducerExporterOptions } from './kafkaJSProducerExporterOptions'
 
+/**
+ * Exposes metrics for a KafkaJS producer in prometheus format.
+ *
+ * @param producer The KafkaS producer to monitor.
+ * @param clientId The client id for the application. The client id is identical across different instances of the same application.
+ * @param register The prometheus registry used to expose the metrics.
+ * @param options Optional parameter to configure the exporter
+ */
 export function monitorKafkaJSProducer (producer: Producer, clientId: string, register: Registry, options?: KafkaJSProducerExporterOptions): void {
   const exporter = new KafkaJSProducerPrometheusExporter(producer, clientId, register, options)
   exporter.enableMetrics()
