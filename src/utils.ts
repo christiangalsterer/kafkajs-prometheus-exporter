641e6ae2972953e31c1acde2668c311b816dcf8a
Christian Galsterer <christian.galsterer@gmx.de>
2023-10-21 22:37:11 +0200

    build: run snyk code test only if there are changes in src or test files (test)

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Merges an array of label names with the label names of the default labels into a new array.
+ * @param labelNames array of label names to merge with the default labels
+ * @param defaultLabels  default labels to merge with
+ * @returns array of merged label names
+ */
 export function mergeLabelNamesWithStandardLabels (labelNames: string[], defaultLabels?: Record<string, string | number>): string[] {
   let merged: string[]
   defaultLabels !== undefined ? merged = labelNames.concat(Object.keys(defaultLabels)) : merged = labelNames
