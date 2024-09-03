ee8081de3a7413967343332faadea2261f931b90
Christian Galsterer <christian.galsterer@gmx.de>
2024-09-03 16:32:25 +0200

    test: added integrations test for kafka producer connection metrics using testcontainers

---
+++ test/kafkaJSProducerPrometheusExporterIT.spec.ts
@@ -0,0 +1,56 @@
+import { beforeEach, describe, expect, test } from '@jest/globals'
+import { KafkaContainer, type StartedKafkaContainer } from '@testcontainers/kafka'
+import { Kafka, type Producer } from 'kafkajs'
+import { Registry } from 'prom-client'
+
+import { monitorKafkaJSProducer } from '../src/monitorKafkaJSProducer'
+
+describe('it kafkaJSProducerPrometheusExporter', () => {
+  const clientId = 'myTestClientId'
+  const KAFKA_PORT = 9093
+  let register: Registry
+  let producer: Producer
+  let kafka: Kafka
+  let kafkaContainer: StartedKafkaContainer
+
+  beforeAll(async () => {
+    kafkaContainer = await new KafkaContainer().withExposedPorts(KAFKA_PORT).start()
+  }, 60000)
+
+  afterAll(async () => {
+    await kafkaContainer.stop()
+  })
+
+  beforeEach(() => {
+    const broker: string = kafkaContainer.getHost() + ':' + kafkaContainer.getMappedPort(KAFKA_PORT).toString()
+    register = new Registry()
+    kafka = new Kafka({
+      clientId,
+      brokers: [broker]
+    })
+    producer = kafka.producer()
+
+    monitorKafkaJSProducer(producer, register)
+  })
+
+  test('it kafka producer connection metrics', async () => {
+    await producer.connect()
+
+    const kafkaProducerConnectionCount = await register.getSingleMetric('kafka_producer_connection_count')?.get()
+    expect(kafkaProducerConnectionCount?.type).toEqual('gauge')
+    expect(kafkaProducerConnectionCount?.values.length).toEqual(1)
+    expect(kafkaProducerConnectionCount?.values.at(0)?.value).toEqual(1)
+
+    const kafkaProducerConnectionCreationTotal = await register.getSingleMetric('kafka_producer_connection_creation_total')?.get()
+    expect(kafkaProducerConnectionCreationTotal?.type).toEqual('counter')
+    expect(kafkaProducerConnectionCreationTotal?.values.length).toEqual(1)
+    expect(kafkaProducerConnectionCreationTotal?.values.at(0)?.value).toEqual(1)
+
+    await producer.disconnect()
+
+    const kafkaProducerConnectionCloseTotal = await register.getSingleMetric('kafka_producer_connection_close_total')?.get()
+    expect(kafkaProducerConnectionCloseTotal?.type).toEqual('counter')
+    expect(kafkaProducerConnectionCloseTotal?.values.length).toEqual(1)
+    expect(kafkaProducerConnectionCloseTotal?.values.at(0)?.value).toEqual(1)
+  })
+})
